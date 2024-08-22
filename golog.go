d8f4818d633d0f68c08803b255570ecbc790fa2c
haesooKr <gotn676@gmail.com>
2024-08-22 14:07:50 -0400

    테스트: Info / Debug 테스트

--- golog.go
+++ golog.go
@@ -2,6 +2,16 @@ package golog
 
 import "fmt"
 
+var (
+	Info *log.Logger
+	Debug *log.Logeger
+)
+
+func init(){
+	Info = log.New(os.Sterr, "INFO\t", log.LstdFlags)
+	Debug = log.New(os.Stderr, "DEBUG\t", log.LstdFlags)
+}
+
 func Log(message string) {
 	fmt.Println("GOLOG: ", message)
 }
